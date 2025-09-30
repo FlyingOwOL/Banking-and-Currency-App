8ab167e2588325e011aca6a875f80a377bd20727
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-09-30 15:19:52 +0800

    display the South East Asians

--- src/Test.js
+++ src/Test.js
@@ -1,3 +1,4 @@
-function supa(){
-    console.log("supa");
-}
+import * as display from "./display_choices.js";
+
+
+display.interestAmount("Savings Account", 10000, "PHP", 0.03, 30);
---
+++ src/display_choices.js
@@ -0,0 +1,98 @@
+export function mainMenu(){
+    console.log(
+    `Select Transaction: 
+    [1] Register Account Name 
+    [2] Deposit Amount 
+    [3] Withdraw Amount 
+    [4] Currency Exchange 
+    [5] Record Exchange Rates 
+    [6] Show Interest Computation`);
+}
+
+export function registerAccountName(lastName, firstName){
+    console.log(
+    `Register Account Name 
+    Account Name: ${lastName}, ${firstName}  
+    Back to the Main Menu (Y/N):`);
+}
+
+export function depositAndWithdrawAmount(transactionType, accountName){
+    console.log(
+    `${transactionType} Amount 
+    Account Name: ${accountName} 
+    Current Balance: 1000.00 
+    Currency: PHP 
+    
+    ${transactionType} Amount: 500.00 
+    Updated Balance: 1500.00 
+    
+    Back to the Main Menu (Y/N):`);
+}
+
+/**
+ * Used to display the currency choices
+ * @returns {string} currency choices to polarize in logger
+ */
+function displayCurrencies(){
+    const currencyChoices =
+    `[1] Philippine Peso (PHP) 
+    [2] United States Dollar (USD) 
+    [3] Japanese Yen (JPY) 
+    [4] British Pound Sterling (GBP) 
+    [5] Euro (EUR) 
+    [6] Chinese Yuan Renminni (CNY)`;
+    return currencyChoices;
+}
+
+export function recordExchangeRates(){
+    console.log(
+    `Record Exchange Rate
+
+    ${displayCurrencies()}
+    
+    Select Foreign Currency: [2] 
+    Exchange Rate: 52.00 
+    
+    Back to the Main Menu (Y/N): `);
+} 
+ 
+/**
+ * Used on the first half of the currency exchange display
+ * @returns {string} source currency display to polarize in logger
+ */
+function sourceCurrencyDisplay(){
+    const sourceCurrency =
+    `Foreign Currency Exchange
+    Source Currency Option:
+    ${displayCurrencies()}`;
+    return sourceCurrency
+}
+
+export function currencyExchange(){
+    console.log(
+    `${sourceCurrencyDisplay()}
+
+Source Currency: [2] 
+Source Amount: 100.00
+
+Exchanged Currency Options:
+    ${displayCurrencies()}
+    
+Exchanged Currency: [1]
+Exchanged Amount: 5200.00
+
+Convert another currency (Y/N)?: `);
+}
+
+export function interestAmount(accountName, balance, currency, interestRate, totalDays){
+    console.log(
+    `Show Interest Amount 
+    Account Name: ${accountName} 
+    Current Balance: ${balance} 
+    Currency: ${currency} 
+    Interest Rate: ${interestRate}% 
+
+    Total Number of Days: ${totalDays}
+    
+    Back to the Main Menu (Y/N): `);
+}
