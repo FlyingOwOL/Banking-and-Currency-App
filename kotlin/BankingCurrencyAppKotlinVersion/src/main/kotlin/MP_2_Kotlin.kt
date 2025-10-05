33af34d50dff1ae3d7def44ded81232cfe737406
Sorrypre <jjlespada5@gmail.com>
2025-10-05 22:56:23 +0800

    added data validation and fixed logic for the conversion

--- kotlin/BankingCurrencyAppKotlinVersion/src/main/kotlin/MP_2_Kotlin.kt
+++ kotlin/BankingCurrencyAppKotlinVersion/src/main/kotlin/MP_2_Kotlin.kt
@@ -10,12 +10,53 @@ import java.math.RoundingMode
 import java.math.BigDecimal
 
 fun main() {
-    displayMenu()
-    println("")
-    displayCurrencyMenu()
+    var account: Account? = null
+    var userInput: Int
+    do {
+        displayMenu()
+        print("Enter your choice (1-7): ")
+        userInput = readln().toIntOrNull() ?: -1
+        when (userInput){
+            1 -> {
+                var willContinue: String
+                do {
+                    println("Register Account Name")
+                    if (account == null) account = createAccount()
+                    else println("Account already exists.")
+                    print("Back to the Main Menu? (Y/N): ")
+                    willContinue = askToProceedMainMenu()
+                } while (willContinue == "N")
+            }
+            2 -> {
+                println("case 2")
+            }
+            7 -> {
+                println("Exiting the program. Goodbye!")
+            }
+            else -> {
+                println("Invalid input. Please enter a number between 1 and 7.\n")
+            }
+        }
+    }while (userInput != 7)
+
 }
 //extension function to format Big Decimal values to specified decimal places
 fun BigDecimal.formatDecimal(digits: Int): String = this.setScale(digits, RoundingMode.HALF_UP).toPlainString()
+
+fun createAccount() : Account? {
+    print("Account Name (Leave blank for Default User): ")
+    val name = readln().trim().ifEmpty {"Default User"}
+    return Account(name)
+}
+
+fun askToProceedMainMenu() : String {
+    print("Back to the Main Menu? (Y/N): ")
+    do {
+        val willContinue = readln().uppercase().trim()
+        if (willContinue != "Y" && willContinue != "N") print("Invalid input. Please enter \"Y\" or \"N\": ")
+        else return willContinue
+    } while (true)
+}
 
 fun displayMenu() {
     val options = arrayOf("Register Account Name", "Deposit Amount", "Withdraw Amount", "Currency Exchange", "Record Exchange Rates", "Show Interest Computation", "Exit")
@@ -23,6 +64,7 @@ fun displayMenu() {
     for ((index, option) in options.withIndex()) {
         println("[${index + 1}] $option")
     }
+    println("\n")
 }
 
 fun displayCurrencyMenu() {
@@ -32,7 +74,6 @@ fun displayCurrencyMenu() {
         println("[${index + 1}] ${currency.fullName} (${currency.code})")
     }
 }
-
 
 //enum class for supported currencies
 enum class Currency(val fullName: String, val code: String) {
@@ -46,11 +87,11 @@ enum class Currency(val fullName: String, val code: String) {
 
 //Account class with basic banking functionalities
 class Account(val name: String, val baseCurrency: Currency = Currency.PHP) {
-    private var balance = BigDecimal.ZERO
+    private var balance = 0.00.toBigDecimal()
 
     fun deposit(amount: BigDecimal, currency: Currency) {
         // For simplicity, ignoring currency conversion
-        if (amount > BigDecimal("0.00")) balance += amount else println("Deposit amount must be positive")
+        if (amount > 0.00.toBigDecimal()) balance += amount else println("Deposit amount must be positive")
     }
     fun withdraw(amount: BigDecimal, currency: Currency) {
         // For simplicity, ignoring currency conversion
@@ -73,8 +114,11 @@ class Account(val name: String, val baseCurrency: Currency = Currency.PHP) {
 //ConversionRate class to handle currency conversion
 // SOURCE CURRENCY, BASE CURRENCY, EXCHANGE RATE
 // Example : USD -> GBP will use USD->PHP and PHP->GBP since PHP is the base currency
-data class ConversionRate(val source: Currency, val dest: Currency, var rate: Double) {
-    fun calculateConversion(amount: Double): Double {
+data class ConversionData(val source: Currency, val dest: Currency, var rate: BigDecimal) {
+    fun convert(amount: BigDecimal): BigDecimal {
         return amount * rate
+    }
+    fun sourceToDestination(amount: BigDecimal): BigDecimal {
+        return amount / rate
     }
 }
