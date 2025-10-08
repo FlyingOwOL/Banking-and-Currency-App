49d26fbabe767a839998e0064fb682576144b538
deppy0 <61898784+deppy0@users.noreply.github.com>
2025-10-09 00:08:44 +0800

    Added REQ1-5,9 - WIP 60%

--- r/MP_4_R.r
+++ r/MP_4_R.r
@@ -1,12 +1,11 @@
-# FOREX as of Oct 7, 2025 00:15H
-# Gathered from https://www.oanda.com/currency-converter/en
-CURRENCIES<-c("PHP", "USD", "JPY", "GBP", "EUR", "CNY")
-PHP_RATIOS<-c(1.00000,	0.01725,	2.54734,	0.01280,	0.01469,	0.12278)
-USD_RATIOS<-c(57.8234,	1.00000,	147.70200,	0.74190,	0.85171,	7.11900)
-JPY_RATIOS<-c(0.39142,	0.00677,	1.00000,	0.00502,	0.00577,	0.04819)
-GBP_RATIOS<-c(77.8872,	1.34698,	198.97200,	1.00000,	1.14733,	9.58918)
-EUR_RATIOS<-c(67.8778,	1.17388,	173.384,	0.87101,	1.00000,	8.35686)
-CNY_RATIOS<-c(8.12127,	0.14045,	20.7447,	0.10420,	0.11962,	1.00000)
+# Currency
+CURRENCIES<-c("PHP","USD","JPY","GBP","EUR","CNY")
+CURRENCY_RATES<-array(0.0,dim=c(length(CURRENCIES),length(CURRENCIES)))
+
+# user
+ACCOUNT_NAME<-NULL
+ACCOUNT_BALANCE<-100.0
+ACCOUNT_CURRENCY<-CURRENCIES[1]
 
 # For reading inputs
 prompter <- function(placeholder) {
@@ -21,100 +20,224 @@ prompter <- function(placeholder) {
 # For sending outputs
 printer <- function(msg) { if (interactive()) print(msg) else cat(msg) }
 
-currency_converter <- function(amount,from,to) {
-	target_from<-switch(from,
-		PHP_RATIOS,
-		USD_RATIOS,
-		JPY_RATIOS,
-		GBP_RATIOS,
-		EUR_RATIOS,
-		CNY_RATIOS
-	)
-	return(target_from[to] * amount)
-}
-
-# Currency conversion
-state1 <- function() {
-	digit_from<-0
-	digit_to<-0
-	# Prompt user-desired conversion
-	while (digit_from<1 || digit_from>6 || digit_to<1 || digit_to>6 || digit_from==digit_to) {
+# For prompting user about which currency
+currency_selector <- function(header_text) {
+	prompt_ret<-NA
+	while (is.na(prompt_ret)||prompt_ret>length(CURRENCIES)||prompt_ret<1) {
 		prompt_ret<-as.numeric(prompter(paste(sep="",
-			"\n",
-			"Enter a two-digit number with each digit corresponding to the choices below.\n",
-			"Note that you cannot enter same digits, e.g. 11, 22, 33, ...\n",
-			"1) Philippine Peso (PHP)\n",
-			"2) United States Dollar (USD)\n",
-			"3) Japanese Yen (JPY)\n",
-			"4) British Pound (GBP)\n",
-			"5) Euro (EUR)\n",
-			"6) Chinese Yuan Renminbi (CNY)\n",
-			"7) Return to menu",
-			"\n",
-			">>>"
+			"\n",header_text,"\n",
+			"[1] Philippine Peso (PHP)\n",
+			"[2] United States Dollar (USD)\n",
+			"[3] Japanese Yen (JPY)\n",
+			"[4] British Pound (GBP)\n",
+			"[5] Euro (EUR)\n",
+			"[6] Chinese Yuan Renminbi (CNY)\n",
+			"[7] Return to menu\n",
+			"\n>>>"
 		)))
-		if (!is.na(prompt_ret)) {
-			digit_from<-prompt_ret%/%10
-			digit_to<-prompt_ret%%10
-		}
-		if (digit_from==7 || digit_to==7)
+		if (prompt_ret==7)
 			return(0)
 	}
-	from_amount<-NA
-	# Prompt amount user wants to convert
-	while (is.na(from_amount)) {
-		from_amount<-as.numeric(prompter(paste(sep="",
-			"\nEnter the amount of ",CURRENCIES[digit_from]," to convert to ",CURRENCIES[digit_to],": "
+	return(prompt_ret)
+}
+
+currency_converter <- function(amount,from,to) {
+	if (CURRENCY_RATES[from,to]==0.0)
+		return(NA)
+	else
+		return(CURRENCY_RATES[from,to]*amount)
+}
+
+show_account_status <- function() {
+	printer(paste(sep="",
+		"Account name: ",ACCOUNT_NAME,"\n",
+		"Balance: ",ACCOUNT_BALANCE,"\n",
+		"Currency: ",ACCOUNT_CURRENCY,"\n"
+	))
+}
+
+# Register Account Name
+state1 <- function() {
+	# Reset and prompt for new account Name
+	ACCOUNT_NAME<<-NULL
+	while (is.null(ACCOUNT_NAME)) {
+		ACCOUNT_NAME<<-prompter("Set a valid account name: ")
+	}
+	printer(paste(sep="","Account name has been set to '",ACCOUNT_NAME,"'.\n"))
+	# Console pause
+	prompter("\nPress ENTER to return to main menu...")
+	return(0)
+}
+
+# Deposit Amount
+state2 <- function() {
+	show_account_status()
+	# Prompt for amount
+	last_balance<-ACCOUNT_BALANCE
+	deposit<-NA
+	while (is.na(deposit)||deposit<=0.0) {
+		deposit<-as.numeric(prompter(paste(sep="",
+			"\n",
+			"Specify deposit amount: "
 		)))
 	}
+	# Add to balance and inform
+	ACCOUNT_BALANCE<<-ACCOUNT_BALANCE+deposit
+	printer(paste(sep="",
+		"\n",
+		"The balance of ",ACCOUNT_NAME," has been updated from ",
+		last_balance," to ",ACCOUNT_BALANCE," ",ACCOUNT_CURRENCY,
+		" by depositing ",deposit," ",ACCOUNT_CURRENCY,".\n"
+	))
+	# Console pause
+	prompter("\nPress ENTER to return to main menu...")
+	return(0)
+}
+
+# Withdraw Amount
+state3 <- function() {
+	show_account_status()
+	# Prompt for amount
+	last_balance<-ACCOUNT_BALANCE
+	withdraw<-NA
+	while (is.na(withdraw)||withdraw<=0.0||withdraw>last_balance) {
+		withdraw<-as.numeric(prompter(paste(sep="",
+			"\n",
+			"Specify withdraw amount: "
+		)))
+	}
+	# Subtract from balance and inform
+	ACCOUNT_BALANCE<<-ACCOUNT_BALANCE-withdraw
+	printer(paste(sep="",
+		"\n",
+		withdraw," ",ACCOUNT_CURRENCY," has been taken from ",ACCOUNT_NAME,".\n",
+		"The updated balance is now from ",last_balance," to ",ACCOUNT_BALANCE," ",ACCOUNT_CURRENCY,".\n"
+	))
+	# Console pause
+	prompter("\nPress ENTER to return to main menu...")
+	return(0)
+}
+
+# Currency Exchange
+state4 <- function() {
+	# Prompt user-desired conversion
+	digit_from<-currency_selector("Choose what currency to convert from:")
+	if (digit_from==0)
+		return(digit_from)
+	
+	digit_to<-digit_from
+	while (digit_to==digit_from) {
+		digit_to<-currency_selector(paste(sep="",
+			"Choose what currency to convert to, except for ",
+			digit_from,") ",CURRENCIES[digit_from],":"
+		))
+		if (digit_to==0)
+			return(digit_to)
+	}
+	
+	# Check if there is a conversion record
+	if (is.na(currency_converter(1,digit_from,digit_to))) {
+		printer(paste(sep="",
+			"There is no conversion rate assigned for ",
+			CURRENCIES[digit_from]," to ",CURRENCIES[digit_to]," yet.\n",
+			"Please assign a conversion rate on the Record Exchange Rates menu."
+		))
+		prompter("\nPress ENTER to return to main menu...")
+		return(0)
+	}
+	
+	# Prompt amount user wants to convert
+	from_amount<-NA
+	printer(paste(sep="",
+		"The current rate from ",CURRENCIES[digit_from]," to ",CURRENCIES[digit_to],
+		" is ",CURRENCY_RATES[digit_from,digit_to],".\n"
+	))
+	while (is.na(from_amount)) {
+		from_amount<-as.numeric(prompter(paste(sep="",
+			"\nEnter the amount of ",CURRENCIES[digit_from],
+			" to convert to ",CURRENCIES[digit_to],": "
+		)))
+	}
+	
 	# Print results
 	printer(paste(sep="",
 		from_amount," ",CURRENCIES[digit_from]," is equivalent to ",
-		currency_converter(from_amount, digit_from, digit_to)," ",CURRENCIES[digit_to],"."
+		currency_converter(from_amount,digit_from,digit_to)," ",CURRENCIES[digit_to],".\n"
 	))
+	
 	# Console pause
-	prompter("\nPress ENTER to continue...")
-	return(1)
+	prompter("\nPress ENTER to make another currency exchange...")
+	return(4)
 }
 
-# Balance
-state2 <- function() {
-	
-}
-
-# Forecast interest
-state3 <- function() {
-	
-}
-
-# Withdraw
-state4 <- function() {
-	
-}
-
-# Deposit
+# Record Exchange Rates
 state5 <- function() {
+	# Prompt user-desired conversion
+	digit_from<-currency_selector("Choose what currency to convert from:")
+	if (digit_from==0)
+		return(digit_from)
 	
+	digit_to<-digit_from
+	while (digit_to==digit_from) {
+		digit_to<-currency_selector(paste(sep="",
+			"Choose what currency to convert to, except for ",
+			digit_from,") ",CURRENCIES[digit_from],":"
+		))
+		if (digit_to==0)
+			return(digit_to)
+	}
+	
+	# Assign rate
+	rate<-NA
+	if (CURRENCY_RATES[digit_from,digit_to]!=0)
+		printer(paste(sep="",
+			"The current rate from ",CURRENCIES[digit_from]," to ",CURRENCIES[digit_to],
+			" is ",CURRENCY_RATES[digit_from,digit_to],".\n"
+		))
+	while (is.na(rate)||rate<=0.0) {
+		rate<-as.numeric(prompter(paste(sep="",
+			"Assign a ",ifelse(CURRENCY_RATES[digit_from,digit_to]==0.0,"new rate","rate")," from ",
+			CURRENCIES[digit_from]," to ",CURRENCIES[digit_to],": "
+		)))
+	}
+	
+	# Change rates to and back, then print completion.
+	CURRENCY_RATES[digit_from,digit_to]<<-rate
+	CURRENCY_RATES[digit_to,digit_from]<<-rate^(-1)
+	printer(paste(sep="",
+		"Currency exchange for ",CURRENCIES[digit_from]," and ",CURRENCIES[digit_to],
+		" has been updated.\n"
+	))
+	
+	# Console pause
+	prompter("\nPress ENTER to make another record...")
+	return(5)
+}
+
+# Show Interest Computation
+state6 <- function() {
+	return(0)
 }
 
 # Screen state transferer
 runState <- function(state=0) {
 	if (state==0) {
-		prompt_ret<-prompter(paste(sep="",
+		prompt_ret<-as.numeric(prompter(paste(sep="",
 			"\n",
-			"Enter a transaction of your choice:\n",
-			"1) Currency conversion\n",
-			"2) Balance\n",
-			"3) Forecast interest\n",
-			"4) Withdraw\n",
-			"5) Deposit\n",
-			"6) Exit\n",
-			"\n",
-			">>>"
-		))
-		if (prompt_ret>0 && prompt_ret<=5)
+			"Select transaction:\n",
+			"[1] Register Account Name\n",
+			"[2] Deposit Amount\n",
+			"[3] Withdraw Amount\n",
+			"[4] Currency Exchange\n",
+			"[5] Record Exchange Rates\n",
+			"[6] Show Interest Computation\n",
+			"[7] Exit\n",
+			"\n>>>"
+		)))
+		
+		if (prompt_ret>=1&&prompt_ret<=6)
 			return(prompt_ret)
-		else if (prompt_ret==6)
+		else if (prompt_ret==7)
 			return(-1)
 		else
 			return(0)
@@ -124,15 +247,20 @@ runState <- function(state=0) {
 			state2(),
 			state3(),
 			state4(),
-			state5()
+			state5(),
+			state6()
 		))
 	}
 }
 
 main <- function() {
 	state<-0;
-	while (state!=-1)
-		state<-runState(state)
+	while (state!=-1) {
+		if (is.null(ACCOUNT_NAME))
+			state<-state1()
+		else
+			state<-runState(state)
+	}
 }
 
 main()
