52f54e8b3cda778e4d2c519acc5f0ad80622dadc
Sorrypre <jjlespada5@gmail.com>
2025-11-29 11:27:19 +0800

    fixed the damn build error

--- kotlin/BankingCurrencyAppKotlinVersion/build.gradle.kts
+++ kotlin/BankingCurrencyAppKotlinVersion/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.1.20"
 }
 
+
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
@@ -12,7 +13,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 }
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MP_4_KotlinKt"
+    }
+    from(sourceSets.main.get().output)
 
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })}
 tasks.test {
     useJUnitPlatform()
 }
